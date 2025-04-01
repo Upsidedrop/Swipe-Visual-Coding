9595bcd783c22a1c02d546ad7b939a27c3e341a0
Recurring Oddity <danashby2000@gmail.com>
2025-04-01 11:32:48 -0400

    0.08.01

---
+++ include/Button.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <functional>
+
+#include "Entity.hpp"
+#include "Collider.hpp"
+
+template <typename T>
+class Button : Entity{
+    Button(Vector2f p_pos, SDL_Texture* p_tex, std::map<int, std::unordered_set<Entity*>>* p_layers, SDL_Rect p_frame, Vector2f p_scale = Vector2f(1,1), int p_layer = 0, std::function<T> p_func, SDL_FRect p_colliderRect, vector<vector<Collider*>>* colliderFlags)
+    :Entity(p_pos, p_tex, p_layers, p_frame, p_scale, p_layer), func(p_func)
+    {
+        collider = new Collider(p_colliderRect, this, colliderFlags, 4);
+    }
+    void CallFunc(){
+        func();
+    }
+    ~Button(){
+        delete collider;
+    }
+    private:
+    std::function<T> func;
+    Collider* collider;
+};
