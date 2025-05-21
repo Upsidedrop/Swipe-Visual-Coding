307775484d1ce834c2a8c5fd6b3bbc31cd6ee641
Recurring Oddity <danashby2000@gmail.com>
2025-05-21 08:06:50 -0400

    0.10.14

--- include/FuncHead.hpp
+++ include/FuncHead.hpp
@@ -10,17 +10,20 @@ class FuncHead;
 extern std::unordered_map<std::string, FuncHead*> functions;
 
 class FuncHead : public Block{
+    const int MIDDLE_TEXTURE_SIZE = 50;
+
     public:
-    FuncHead(Vector2f p_pos, float p_scale, SDL_Texture* p_tex, BlockType p_type, SDL_Rect p_frame, std::string p_name);
+    FuncHead(Vector2f p_pos, float p_scale, SDL_Texture* p_tex, BlockType p_type, SDL_Rect p_frame, std::string p_name, Vector2f p_textOffset);
     void setPos(Vector2f p_pos) override
     {
         pos = p_pos;
+        //currentFrame > -2 < is to account for dip at bottom of block and top of other block
         if(child != nullptr)
         {
-            child-> setPos(p_pos + Vector2f(0, 13 * scale.y));
+            child-> setPos(p_pos + Vector2f(0, (currentFrame.h - 2) * scale.y));
         }
-        text.getVisual() -> setPos(p_pos + Vector2f(10, 5));
-        middle -> setPos(Vector2f(p_pos.x + 13 * scale.x, p_pos.y));
-        end -> setPos(Vector2f(p_pos.x + 13 * scale.x + text.getDimensions().x, p_pos.y));
+        text.getVisual() -> setPos(p_pos + Vector2f(10, 7)); 
+        middle -> setPos(Vector2f(p_pos.x + currentFrame.w * scale.x, p_pos.y));
+        end -> setPos(Vector2f(p_pos.x + currentFrame.w * scale.x + MIDDLE_TEXTURE_SIZE * middle->getScale().x, p_pos.y));
     }
 };
--- src/funchead.cpp
+++ src/funchead.cpp
@@ -1,14 +1,14 @@
 #include "FuncHead.hpp"
 
-FuncHead::FuncHead(Vector2f p_pos, float p_scale, SDL_Texture* p_tex, BlockType p_type , SDL_Rect p_frame, std::string p_name)
-:Block(p_pos, p_tex, p_frame, p_type, p_scale)
+FuncHead::FuncHead(Vector2f p_pos, float p_scale, SDL_Texture* p_tex, BlockType p_type , SDL_Rect p_frame, std::string p_name, Vector2f p_textOffset)
+:Block(p_pos, p_tex, p_frame, p_type, p_scale, p_name.c_str(), p_textOffset)
 {
     delete topCollider;
     topCollider = nullptr;
     
     SDL_FRect bottomColRect;
     bottomColRect = bottomCollider->GetFrame();
-    bottomColRect.y += 2;
+    bottomColRect.y += 3;
     bottomCollider -> SetFrame(bottomColRect);
 
     functions[p_name] = this;
