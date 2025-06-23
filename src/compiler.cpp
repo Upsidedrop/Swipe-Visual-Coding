23c5a5ded97e1db4e0c22a81e8bb45ed1474ed65
Recurring Oddity <danashby2000@gmail.com>
2025-06-23 16:15:57 -0400

    0.12.15

---
+++ src/compiler.cpp
@@ -0,0 +1,38 @@
+#include "Compiler.hpp"
+#include "Loop.hpp"
+
+namespace Compilation{
+    std::string prefPath = SDL_GetPrefPath("Oddity", "Swipe");
+    std::ofstream program;
+    
+    void TypeChecker(Block* it, char foo){
+        if(it == nullptr){
+            return;
+        }
+        if(it -> GetType() == BlockType::DEFAULT){
+            program << "std::cout << \"Hello World!\\n\";\n";
+        }
+        if(it -> GetType() == BlockType::DEFAULTLOOP){
+            program << "for(int " << foo << " =0;" << foo << "<3;++" << foo <<"){\n";
+            TypeChecker(((Loop*)(it)) -> GetInnerChild(), foo + 1);
+            program << "}\n";
+        }
+        TypeChecker(it -> getChild(), foo);
+    }
+
+    void Compile(){
+        program.open(prefPath + "program.cpp");
+
+        program << "#include <iostream> \nint main(){\n";
+
+        TypeChecker(functions["main"], 'a');
+
+        program << "}";
+
+        program.close();
+
+        std::string command = "cd " + prefPath + " && g++ program.cpp -Wall && ./a.out";
+
+        system(command.c_str());
+    }
+}
