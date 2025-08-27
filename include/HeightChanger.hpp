a0d648858da08819c5c04f9207e45998fc1dde47
Recurring Oddity <danashby2000@gmail.com>
2025-08-27 19:30:58 -0400

    0.15.01 Began UpdateHeight function that will be used by variables and blocks

--- include/HeightChanger.hpp
+++ include/HeightChanger.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
-#include "Entity.hpp"
+#include "DividedEntity.hpp"
 
 class HeightChanger{
-    HeightChanger(
-        
-    );
+    HeightChanger(DividedEntity& p_parts): parts(p_parts){};
+    DividedEntity& parts;
+    void UpdateHeight(int height){
+        height -= parts.top -> getCurrentFrame().h * parts.top -> getScale().y;
+        height -= parts.bottom -> getCurrentFrame().h * parts.bottom -> getScale().y;
+
+        parts.left -> setScale(Vector2f(parts.left -> getScale().x, height / parts.left -> getCurrentFrame().h));
+        parts.center -> setScale(Vector2f(parts.center -> getScale().x, parts.left -> getScale().y));
+        parts.right -> setScale(parts.left -> getScale());
+
+        parts.bottomLeft -> setPos(Vector2f(parts.bottomLeft -> getPos().x, parts.left -> getPos().y + height));
+        parts.bottom -> setPos(Vector2f(parts.bottom->getPos().x, parts.bottomLeft -> getPos().y));
+        parts.bottomRight -> setPos(Vector2f(parts.bottomRight->getPos().x, parts.bottomLeft -> getPos().y));
+    }
 };
