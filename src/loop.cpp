64c0c193bd385b97be8fb2d92d7083f90a0eac21
Recurring Oddity <danashby2000@gmail.com>
2025-09-04 14:47:27 -0400

    0.15.08 Aligned footEnd to correct spot relative to footMiddle

--- src/loop.cpp
+++ src/loop.cpp
@@ -36,10 +36,10 @@ Loop::Loop(Vector2f p_pos, float p_scale, SDL_Texture* p_tex, BlockType p_type, 
 
     float a = (footFrame.x + footFrame.w) * p_scale;
 
-    float adjustedMiddleScale = (currentFrame.w + parts.topRight -> getCurrentFrame().w - footEndFrame.w - footFrame.w) * p_scale + parts.top -> getCurrentFrame().w * parts.top -> getScale().x / (footMiddleFrame.w) * p_scale;
+    float adjustedMiddleScale = (currentFrame.w + parts.topRight -> getCurrentFrame().w - footEndFrame.w - footFrame.w) * p_scale + parts.top -> getCurrentFrame().w * parts.top -> getScale().x / (footMiddleFrame.w * p_scale);
 
     footMiddle = new Entity(Vector2f(a, DEFAULT_LOOP_SIZE * p_scale) + p_pos, p_tex, footMiddleFrame, Vector2f(adjustedMiddleScale, p_scale));
-    footEnd = new Entity(Vector2f(parts.topRight -> getPos().x, DEFAULT_LOOP_SIZE * p_scale + p_pos.y), p_tex, footEndFrame, Vector2f(p_scale, p_scale));
+    footEnd = new Entity(Vector2f(footMiddle -> getScale().x * footMiddleFrame.w + footMiddle -> getPos().x, DEFAULT_LOOP_SIZE * p_scale + p_pos.y), p_tex, footEndFrame, Vector2f(p_scale, p_scale));
 
     SDL_FRect bottomColFrame;
     bottomColFrame.x = 0;
