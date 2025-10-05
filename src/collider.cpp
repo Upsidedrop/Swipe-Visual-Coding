f0bc42446971cc8ab362fad4e0c678ef767021b9
Recurring Oddity <danashby2000@gmail.com>
2025-10-05 12:47:14 -0400

    0.15.36 * variable now prioritizes gap closest to its top left corner

--- src/collider.cpp
+++ src/collider.cpp
@@ -1,8 +1,14 @@
 #include <vector>
+#include <math.h>
 
 #include "Collider.hpp"
 
+constexpr int GAP_ANCHOR_OFFSET = 5;
+
 Collider* Collider::CheckForCollisions(std::vector<int> masks, std::function<bool(Collider*)> p_predicate){
+    Collider* closest = nullptr;
+    int min = -1;
+    Vector2f adjustedPos = parent -> getPos() - Vector2f(GAP_ANCHOR_OFFSET, 0);
     for(int i : masks){
         for(Collider* other : flags[i]){
             if(p_predicate(other)){
@@ -23,10 +29,14 @@ Collider* Collider::CheckForCollisions(std::vector<int> masks, std::function<boo
             if((parent -> getPos().y + (frame.h + frame.y) * parent -> getScale().y) < (other -> GetParent() -> getPos().y) + other -> GetFrame().y * other -> GetParent() -> getScale().y){
                 continue;
             }
-            return other;
+            int distance = pow(adjustedPos.x - other -> GetParent() -> getPos().x, 2) + pow(adjustedPos.y - other -> GetParent() -> getPos().y, 2);
+            if(distance < min || min == -1){
+                closest = other;
+                min = distance;
+            }
         }
     }
-    return nullptr;
+    return closest;
 }
 Collider* Collider::CheckForCollisionsHeightPriority(std::vector<int> masks){
     //searches for lowest collider, but since the highest point is at y = 0, we search for a collider with the HIGHEST y position
